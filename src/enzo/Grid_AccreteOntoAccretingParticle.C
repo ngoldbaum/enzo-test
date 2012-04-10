c4b184545af259eb599ba87d97a59d8721212ebd
Nathan Goldbaum <goldbaum@ucolick.org>
2012-04-09 22:52:41 -0700

    Some updates to conserve linear and angular momentum during accretion.
    --HG--
    branch : active_particles

--- src/enzo/Grid_AccreteOntoAccretingParticle.C
+++ src/enzo/Grid_AccreteOntoAccretingParticle.C
@@ -6,6 +6,7 @@
 /  written by: Nathan Goldbaum
 /  date:       April 2012
 /
+/  note:       Equation numbers refer to Krumholz McKee & Klein (2004)
 ************************************************************************/
 
 #include <string.h>
@@ -48,7 +49,7 @@ int grid::AccreteOntoAccretingParticle(ActiveParticleType_AccretingParticle* Thi
     ENZO_FAIL("Error in IdentifyPhysicalQuantities.");
   }
   
-  /* Compute cell width and find botoom left and top right corners of grid */
+  /* Compute cell width and find bottom left and top right corners of grid */
 
   CellSize = CellWidth[0][0];
 
@@ -68,6 +69,7 @@ int grid::AccreteOntoAccretingParticle(ActiveParticleType_AccretingParticle* Thi
       LeftCorner[2] > ParticlePosition[2]+AccretionRadius || RightCorner[2] < ParticlePosition[2]-AccretionRadius)
     return SUCCESS;
 
+  // Eqn 13
   if (ThisParticle->BondiHoyleRadius < CellSize/4.0)
     KernelRadius = CellSize/4.0;
   else if (ThisParticle->BondiHoyleRadius < AccretionRadius/2.0)
@@ -75,9 +77,10 @@ int grid::AccreteOntoAccretingParticle(ActiveParticleType_AccretingParticle* Thi
   else
     KernelRadius = AccretionRadius/2.0;
   
+  // Eqn 12
   RhoInfinity = AverageDensity/bondi_alpha(1.2*CellSize/ThisParticle->BondiHoyleRadius);  
 
-  // See Krumholz et al. 2004 Equation 11
+  // Eqn 11
   ThisParticle->AccretionRate = (4*pi*RhoInfinity*POW(ThisParticle->BondiHoyleRadius,2)*
 				 sqrt(POW(lambda_c*ThisParticle->cInfinity,2) +
 				      POW(ThisParticle->vInfinity,2)));
@@ -93,24 +96,68 @@ int grid::AccreteOntoAccretingParticle(ActiveParticleType_AccretingParticle* Thi
 	  // Subtract mass from this cell
 	  CellMass = BaryonField[DensNum][index]*CellVolume;
 	  Weight = exp(-radius2/(KernelRadius*KernelRadius))/SumOfWeights;
-	  CellSubtractedMass =  this->dtFixed * ThisParticle->AccretionRate * Weight;
-	  if (CellSubtractedMass > 0.25*CellMass) 
-	    CellSubtractedMass = 0.25*CellMass;
-	  SubtractedMass += CellSubtractedMass
-	  BaryonField[DensNum] -= CellSubtracedMass/CellVolume;
+	  CellMassAccreted =  this->dtFixed * ThisParticle->AccretionRate * Weight;
+	  if (CellMassAccreted > 0.25*CellMass) 
+	    CellMassAccreted = 0.25*CellMass;
+	  SubtractedMass += CellMassAccreted
 	  
-	  /* Modify momentum.  Find components of velocity transverse and
-	     perpendicular to teh position of the sink.  Increase the transverse
-	     velocity such that the angular momentum of the gas is unchanged and
-	     leave the radial velocity unchanged. */
-	     
-	  // First find distance to the cell.
+	  /* The true accretion rate is somewhat less than this due to
+	     angular momentum conservation.  Subdivide the cell into
+	     NDIV^2 subcells and estimate the reduction assuming
+	     ballistic orbits. See the discussion near Eqn 15. */
+
+	  /* Find the components of the momentum vector transverse and
+	     perpendicular to the vector connecting the sink and the
+	     cell.  Modify the radial component of the momentum vector
+	     so that momentum is conserved but leave the transverse
+	     component unchanged */
+	    
+	  // Some conveneint shorthand
+	  rhocell = BaryonField[DensNum][index];
+	  mcell = rhocell*CellVolume;
+	  
+
+	  /* Don't worry about conserving angular momentum if we're
+	     accreting no mass from the cell or if we are accreting
+	     all of the mass from it.  Note that paccrete and eaccrete
+	     are total energy and momentum, not energy an momentum
+	     densities. */
+
+	  if ((
+
+	  // Find the radius vector
 	  reff[0] = (CellLeftEdge[0][i] + 0.5*CellWidth[0][i]) - ParticlePosition[0];
 	  reff[1] = (CellLeftEdge[1][j] + 0.5*CellWidth[1][j]) - ParticlePosition[1];
 	  reff[1] = (CellLeftEdge[2][k] + 0.5*CellWidth[2][k]) - ParticlePosition[2];
+	  rsqr = reff[0]*reff[0]+reff[1]*reff[1]+reff[2]*reff[2];
+
+	  // Compute the parallel component of the momentum density
+	  rdotp = reff[0]*pcell[0]+reff[1]*pcell[1]+rcell[2]*pcell[2];
+	  prad[0] = reff[0]*rdotp/rsqr;
+	  prad[1] = reff[1]*rdotp/rsqr;
+	  prad[2] = reff[2]*rdotp/rsqr;
+	    
+	  // Compute the transverse component of the momentum density
+	  ptrans[0] = pcell[0] - prad[0];
+	  ptrans[1] = pcell[1] - prad[1];
+	  ptrans[2] = pcell[2] - prad[2];
+
+	  // Compute the new radial component of the momentum density
+	  pradnew[0] = prad[0]*rhocell/(rhocell+CellSubtractedMass/CellVolume);
+	  pradnew[1] = prad[1]*rhocell/(rhocell+CellSubtractedMass/CellVolume);
+	  pradnew[2] = prad[2]*rhocell/(rhocell+CellSubtractedMass/CellVolume);
+	
+	  // Set the new transverse momentum
+	  ptransnew[0] = ptrans[0];
+	  ptransnew[1] = ptrans[1];
+	  ptransnew[2] = ptrans[2];
+
+	  // Compute the amount of momentum (not momentum density) accreted
+	  paccrete[0] = CellVolume*(pcell[0] - pradnew[0] - ptransnew[0]);
+	  paccrete[1] = CellVolume*(pcell[1] - pradnew[1] - ptransnew[1]);
+	  paccrete[2] = CellVolume*(pcell[2] - pradnew[2] - ptransnew[2]);
 	  
-	  // Compute the radial component of the momentum density
-	  rdotv = 
+	  BaryonField[
 
 	}
       }
