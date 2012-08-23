ec68dfde99b8194d28ff7070282578aec3e02079
Nathan Goldbaum <goldbaum@ucolick.org>
2012-08-23 13:20:31 +0800

    This breaks precision-32 and particles-64.  We need to think more carefully about how to make hdf5 hierarchy files play nicely with variable precision.
    --HG--
    branch : active_particles

--- src/enzo/grid/utilities/Grid_ReadHierarchyInformationHDF5.C
+++ src/enzo/grid/utilities/Grid_ReadHierarchyInformationHDF5.C
@@ -231,7 +231,6 @@ int HDF5_ReadAttribute(hid_t group_id, const char *AttributeName, double &Attrib
 #endif
 
 // FLOAT
-#ifndef CONFIG_BFLOAT_4
 int HDF5_ReadAttribute(hid_t group_id, const char *AttributeName, FLOAT &Attribute, FILE *log_fptr) {
 
   hid_t attr_id;
@@ -254,7 +253,6 @@ int HDF5_ReadAttribute(hid_t group_id, const char *AttributeName, FLOAT &Attribu
 
   return SUCCESS;
 }
-#endif
 
 // int vector
 int HDF5_ReadAttribute(hid_t group_id, const char *AttributeName, int Attribute[], int NumberOfElements, FILE *log_fptr) {
