79e9e526d474bcb790ff67f6be1fa3c5996b3509
Nathan Goldbaum <ngoldbau@ucsc.edu>
2014-12-22 14:56:15 -0800

    Fix compilation error due to internal API change
    --HG--
    branch : enzo-dev

--- src/enzo/headers/ActiveParticle_Skeleton.h
+++ src/enzo/headers/ActiveParticle_Skeleton.h
@@ -83,9 +83,9 @@ public:
    */
   template <class active_particle_class>
   static int BeforeEvolveLevel(HierarchyEntry *Grids[], TopGridData *MetaData,
-				 int NumberOfGrids, LevelHierarchyEntry *LevelArray[],
-				 int ThisLevel, int TotalActiveParticleCountPrevious[],
-				 int SkeletonID)
+      int NumberOfGrids, LevelHierarchyEntry *LevelArray[],
+      int ThisLevel, bool CallEvolvePhotons, int TotalActiveParticleCountPrevious[],
+      int SkeletonID)
     {
       return SUCCESS;
     };
